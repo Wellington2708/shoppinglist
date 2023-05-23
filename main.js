32c0df85ee1e1eb3c2ceb59601fb40aea55dbf68
Wellington2708 <wellingtondevelopercontact@gmail.com>
2023-05-23 19:41:03 +0100

    part 3 of course

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let listaDeItens = [];
 
+let itemAEditar;
+
 const form = document.getElementById("form-itens");
 
 const itensInput = document.getElementById("receber-item");
@@ -58,9 +60,10 @@ function mostrarItens(){
         <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
         <div>
             <input type="checkbox" class="is-clickable" />
-            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+            <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemAEditar) ? `disabled` : ''}></input>
         </div>
         <div>
+            ${ index === Number(itemAEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
     </li>
@@ -78,4 +81,33 @@ function mostrarItens(){
             mostrarItens()
         })
     })
+
+    const deletarObjetos = document.querySelectorAll(".deletar");
+    
+    deletarObjetos.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            valorDoElemento =  evento.target.parentElement.parentElement.getAttribute('data-value');
+            listaDeItens.splice(valorDoElemento, 1);
+            mostrarItens()
+        })
+    })
+
+    const editarItens = document.querySelectorAll(".editar");
+
+    editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar =  evento.target.parentElement.parentElement.getAttribute('data-value');
+            mostrarItens();
+            console.log(listaDeItens)
+        })
+    })
+}
+
+function salvarEdicao(){
+    const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`);
+    //console.log(itemEditado.value);
+    listaDeItens[itemAEditar].valor = itemEditado.value;
+    itemAEditar = -1;
+    mostrarItens()
+    console.log(listaDeItens);
 }
